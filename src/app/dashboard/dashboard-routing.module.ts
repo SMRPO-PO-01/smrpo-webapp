1b46b408a49b3c09b2729c5c7d84c30b4bf92406
Luka Bračun <luka@proxima.si>
2020-03-31 16:51:36 +0200

    add admin guard to admin routes

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from "./dashboard.component";
-import { AddUserComponent } from "./add-user/add-user.component";
+import { AdminGuard } from '../guards/admin.guard';
+import { AddUserComponent } from './add-user/add-user.component';
+import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
   {
     path: "",
     component: DashboardComponent,
+    children: []
+  },
+  {
+    path: "admin",
+    component: DashboardComponent,
+    canActivate: [AdminGuard],
     children: [
       {
         path: "add-user",
@@ -19,6 +26,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AdminGuard]
 })
 export class DashboardRoutingModule {}
