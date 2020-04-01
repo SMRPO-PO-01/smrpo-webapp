c31e51187bb8f8e3790b0e4e1ebc3d0e63d82da5
Luka Bračun <luka@proxima.si>
2020-04-01 22:41:36 +0200

    connected with project-list

--- src/app/dashboard/boards/boards.component.ts
+++ src/app/dashboard/boards/boards.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-
-import { Board } from '../../interfaces/board.interface'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { TaskService } from 'src/app/services/task.service';
 
+import { Board } from '../../interfaces/board.interface';
+
 @Component({
-  selector: 'app-boards',
-  templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.scss']
+  selector: "app-boards",
+  templateUrl: "./boards.component.html",
+  styleUrls: ["./boards.component.scss"]
 })
 export class BoardsComponent implements OnInit {
+  projectId: number;
+  boards: Board[] = [];
 
-  projectId = 1;
-  boards : Board[] = [];
-
-  constructor(private taskService: TaskService) { }
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.initBoards();
+    this.route.params.subscribe(({ id }) => {
+      this.projectId = id;
+      this.initBoards();
+    });
   }
 
   drop(event: CdkDragDrop<string[]>) {
@@ -25,16 +31,18 @@ export class BoardsComponent implements OnInit {
   }
 
   initBoards() {
-    this.getTasks(this.projectId, 'UNASSIGNED');
-    this.getTasks(this.projectId, 'ASSIGNED');
-    this.getTasks(this.projectId, 'ACTIVE');
-    this.getTasks(this.projectId, 'DONE');
+    this.getTasks(this.projectId, "UNASSIGNED");
+    this.getTasks(this.projectId, "ASSIGNED");
+    this.getTasks(this.projectId, "ACTIVE");
+    this.getTasks(this.projectId, "DONE");
   }
 
   getTasks(project: number, state: String) {
-    this.taskService.getTasks({'project': project.toString(), 'search': state}).subscribe(tasks => {
-      this.boards.push({title: state, tasks: tasks})
-    });
+    this.taskService
+      .getTasks({ project: project.toString(), search: state })
+      .subscribe(tasks => {
+        this.boards.push({ title: state, tasks: tasks });
+      });
   }
 
   updateTask(event: CdkDragDrop<string[]>) {
@@ -43,18 +51,25 @@ export class BoardsComponent implements OnInit {
     console.log(taskId);
     console.log(newState);
 
-    this.taskService.updateTask({'id': taskId, 'state': newState}).subscribe(task => {
-      if (task.id = taskId) {
-        if (event.previousContainer === event.container) {
-          moveItemInArray(event.container.data["tasks"], event.previousIndex, event.currentIndex);
-        } else {
-          transferArrayItem(event.previousContainer.data["tasks"],
-                            event.container.data["tasks"],
-                            event.previousIndex,
-                            event.currentIndex);
+    this.taskService
+      .updateTask({ id: taskId, state: newState })
+      .subscribe(task => {
+        if ((task.id = taskId)) {
+          if (event.previousContainer === event.container) {
+            moveItemInArray(
+              event.container.data["tasks"],
+              event.previousIndex,
+              event.currentIndex
+            );
+          } else {
+            transferArrayItem(
+              event.previousContainer.data["tasks"],
+              event.container.data["tasks"],
+              event.previousIndex,
+              event.currentIndex
+            );
+          }
         }
-      }
-    });
+      });
   }
-
 }
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
         component: ProjectListComponent
       },
       {
-        path: "board",
+        path: "project/:id",
         component: BoardsComponent
       }
     ]
